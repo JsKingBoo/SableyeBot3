985a9a4d26b86ea095eb703a71cdf756394fd7ca
Stuart Allan Rutherford <squirrel96@live.co.uk>
2022-01-10 07:02:54 +1100

    Modify //listservers

--- commands/bot_commands/listservers.js
+++ commands/bot_commands/listservers.js
@@ -1,12 +1,14 @@
 'use strict';
 
+const fs = require('fs');
+
 module.exports = {
   desc: "List servers.",
   usage: "",
   options: [{
     name: "verbose",
     value: false,
-    desc: "Lists all servers"
+    desc: "Lists all guilds in a file on the server"
   }],
   adminOnly: true,
   process: async function(msg, flags) {
@@ -14,12 +16,14 @@ module.exports = {
     let servers = bot.guilds.cache;
     sendMsg.push(`Currently watching ${servers.size} server${servers.size === 1 ? '' : 's'}`);
     //Only spit out detailed list while in DM/PM channel
+    const guildList = [];
     if (flags.verbose) {
       servers.forEach(server => {
         const joinedAt = server.joinedAt;
-        sendMsg.push(`[${server.id}] ${server.name} ${joinedAt.getUTCFullYear()}-${joinedAt.getUTCMonth()+1}-${joinedAt.getUTCDate()}`);
+        guildList.push(`[${server.id}] ${server.name} ${joinedAt.getUTCFullYear()}-${joinedAt.getUTCMonth()+1}-${joinedAt.getUTCDate()}`);
       });
     }
+    fs.writeFileSync('logs/servers.txt', guildList.join('\n'));
     return sendMsg;
   }
 };
